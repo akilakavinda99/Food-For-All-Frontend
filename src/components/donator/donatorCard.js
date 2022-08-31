4319dae81d71ae621c7c106234cdf978945439ab
Thushal Shaminda <it20216146@my.sliit.lk>
2022-08-31 23:31:47 +0530

    Created Nav bar

--- src/components/donator/donatorCard.js
+++ src/components/donator/donatorCard.js
@@ -14,6 +14,7 @@ import {
 import img from "./banner.png";
 import swal from "sweetalert";
 import Footer from "../Footer";
+import NavBar from "../NavBar";
 
 export default function DonatorCard() {
   const [donations, setDonations] = useState([]);
@@ -162,6 +163,9 @@ export default function DonatorCard() {
 
   return (
     <>
+    <nav>
+      <NavBar />
+    </nav>
       <div class="card">
         <div class="card-body">
           <ul
