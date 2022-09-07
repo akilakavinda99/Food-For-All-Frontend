9fabc5ca28a98f31d176e6d5e8bc2853fc0622e2
Thushal Shaminda <it20216146@my.sliit.lk>
2022-09-07 08:55:37 +0530

    created requester profile

--- src/components/requester/requesterProfile.js
+++ src/components/requester/requesterProfile.js
@@ -11,7 +11,9 @@ export default function requesterProfile() {
       </nav>
       <h4 className="pt-3 ms-4">Account settings</h4>
       <hr/>
-    <div class="container-signup d-flex justify-content-center pt-4 pb-5">
+    <div class="container d-flex justify-content-center pt-4 pb-5">
+    <div className="card z-index-0 fadeIn3 fadeInBottom ">
+    <div className="card-body">
         <form class="form-control form-profile m-3">
 
         <div class="row pt-5"> 
@@ -25,52 +27,54 @@ export default function requesterProfile() {
             <div class="col ms-2 pe-5 border-end">
                 <div class="row">
                     <div class="col">
-                        <div class="mb-4">
-                            <label for="formFile" class="form-label">First name</label> 
+                        <div class="row input-group input-group-outline mb-4">
+                            <label for="formFile">First name</label> 
                             <input type="text" class="form-control"/> 
                         </div> 
                     </div>
                     <div class="col">
-                        <div class="mb-4"> 
-                            <label for="formFile" class="form-label">Last name</label>
+                        <div class="row input-group input-group-outline mb-4"> 
+                            <label for="formFile">Last name</label>
                             <input type="text" class="form-control"/> 
                         </div>
                     </div>
                 </div>
 
-                <div class="mb-4">
-                    <label for="formFile" class="form-label">Telephone number</label>
+                <div class="row input-group input-group-outline mb-4">
+                    <label for="formFile">Telephone number</label>
                     <input type="text" class="form-control"/> 
                 </div>
 
-                <div class="mb-4"> 
-                    <label for="formFile" class="form-label">Email Address</label>
+                <div class="row input-group input-group-outline mb-4"> 
+                    <label for="formFile" >Email Address</label>
                     <input type="email" class="form-control"/> 
                 </div>
 
             </div>
 
-            <div class="col align-self-end ">
+            <div class="row input-group input-group-outline col align-self-end me-5">
             <div class="mb-4 ms-5">
-                <label for="formFile" class="form-label">Current Password</label>
+                <label for="formFile">Current Password</label>
                 <input type="password" class="form-control"/> 
             </div> 
             <div class="mb-4 ms-5"> 
-                <label for="formFile" class="form-label">New Password</label>
+                <label for="formFile">New Password</label>
                 <input type="password" placeholder="New password" class="form-control"/> 
             </div> 
             <div class="mb-4 ms-5"> 
-                <label for="formFile" class="form-label">Confirm Password</label>
+                <label for="formFile">Confirm Password</label>
                 <input type="password" placeholder="Confirm password" class="form-control"/> 
             </div> 
             </div>
         </div>
             <div class="row pt-5"> 
-              <div class="mb-4 d-flex justify-content-center ps-5 ms-2"> 
+              <div class="mb-4 d-flex justify-content-center"> 
                 <div class="btn btn-primary d-block px-5"> Save changes </div> 
               </div> 
            </div>
         </form>
+      </div>
+      </div>
       </div>
 
   <footer>
