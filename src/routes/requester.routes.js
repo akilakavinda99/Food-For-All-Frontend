cac26d128a652dda29067ab9f1137904dd0259d7
Thushal Shaminda <it20216146@my.sliit.lk>
2022-09-08 17:25:55 +0530

    routes added

--- src/routes/requester.routes.js
+++ src/routes/requester.routes.js
@@ -5,6 +5,9 @@ import RequesterCard from "../components/requester/requesterDashboard";
 import RequesterSignUp from "../components/requester/requesterSignUp";
 import RequesterSignIn from "../components/requester/requesterSignIn"
 import RequesterProfile from "../components/requester/requesterProfile";
+import RequestStepOne from "../components/requester/request/requestStepOne";
+import RequestStepTwo from "../components/requester/request/requestStepTwo";
+import AboutUs from "../components/requester/aboutus";
 
 export default function Requester() {
   return (
@@ -14,6 +17,9 @@ export default function Requester() {
         <Route path="/signup" element={<RequesterSignUp />} />
         <Route path="/signin" element={<RequesterSignIn />} />
         <Route path="/profile" element={<RequesterProfile />} />
+        <Route path="request/step1" element={<RequestStepOne />} />
+        <Route path="request/step2" element={<RequestStepTwo />} />
+        <Route path="/us" element={<AboutUs />} />
       </Routes>
     </>
   )
