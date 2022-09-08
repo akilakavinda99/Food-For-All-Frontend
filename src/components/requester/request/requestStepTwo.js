2257ec9de4e1c69b108d680782b007b3305fc6bf
Thushal Shaminda <it20216146@my.sliit.lk>
2022-09-08 17:25:45 +0530

    request step one and two modified

--- src/components/requester/request/requestStepTwo.js
+++ src/components/requester/request/requestStepTwo.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FileUploader } from 'react-drag-drop-files';
+import "../footer.css"
 import Footer from '../../Footer'
 import NavBar from '../../NavBar'
 
@@ -10,48 +12,40 @@ export default function requestStepTwo() {
       </nav>
 
       <h4 className="pt-3 ms-4">Request Funds</h4>
+      <p className='pt-3 ms-4'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+         It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,</p>
       <hr/>
 
-    <div class="container d-flex justify-content-center pt-5 pb-5">
+    <div class="container d-flex justify-content-center pt-3 pb-5">
       
     <div className="card z-index-0 fadeIn3 fadeInBottom ">
     <div className="card-body">
         <form class="form-control"> 
-          <p class="h3 fw-bold text-center mb-2 pt-4">Sign up for start supporting </p>
-          <p class="text-center mb-5"> Do you work for an organization? signup here</p> 
-
-            <div class="row input-group input-group-outline m-0 px-4"> 
-              <div class="col-md-6 mb-4 ps-0"> 
-                <input class="form-control" type="text" placeholder="First Name"/>
-              </div>
-              <div class="col-md-6 mb-4 pe-0">
-                <input class="form-control" type="text" placeholder="Lirst Name"/>
-              </div> 
-            </div>
+          <p class="h3 fw-bold text-center mb-2 pt-4"> Let's make a request </p>
+          <p class="text-center d-flex ms-3 pt-4 text-muted fw-bold"> Request details </p> 
             
             <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="email" placeholder="Email address" class="form-control"/> 
+              <input type="email" placeholder="Request title" class="form-control"/> 
             </div> 
-            <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="text" placeholder="Telephone number" class="form-control"/> 
+            <div class="input-group input-group-description input-group-outline mb-4 px-4"> 
+              <input type="text" placeholder="Tell your story (Description)" class="form-control"/> 
             </div> 
-            <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="password" placeholder="Password" class="form-control"/> 
-            </div> 
-            <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="password" placeholder="Confirm Password" class="form-control"/> 
-            </div> 
-           
-           
-           <div class="mb-4 px-4"> 
-              <label for="check" class="d-flex align-items-center justify-content-center "/> 
-              <input type="checkbox" id="check"/> 
-              <span class="ms-2 textmuted">I agree with the Terms and Conditions and the privacy policy</span> 
-           </div> 
-          
+
+            <p class="text-center d-flex ms-3 text-muted fw-bold"> Attach documents </p>
+
+          <div className='px-5'>
+            <FileUploader />
+            {/* <p>{imageFile ? `File name: ${imageFile.name}` : "No files uploaded yet"}</p> */}
+            {/* <>{imageFile ?  */}
+             <h7 class="text-muted d-flex"> Accepted file types: JPEG, JPG, PNG  </h7>
+            <img className='rounded img-fluid'/>
+          </div>
+
            <div class="row"> 
               <div class="mb-3 d-flex justify-content-center"> 
-                <div class="btn btn-primary d-block "> SIGN UP </div> 
+                <div class="btn btn-primary d-block "> SUBMIT </div> 
               </div> 
            </div>
            
