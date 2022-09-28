55433393a692dd14d38876b1a322d2633d777897
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-09-28 10:01:12 +0530

    View ongoing funds of the organization

---
+++ src/components/fund/viewFunds/CompletedFunds.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import NoItems from '../../common/noItems/noItems';
+import { getFundByOrganizationAndStatus } from '../../../api/fund.api';
+import CompletedFundCard from './CompletedFundCard';
+
+export default function CompletedFunds(props) {
+
+    return (
+        <>
+        </>
+
+    )
+}
