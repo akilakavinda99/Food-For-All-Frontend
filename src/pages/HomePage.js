b1af3ab0f9a02c662a200b5eec7859d84dcf900a
Thushal Shaminda <it20216146@my.sliit.lk>
2022-09-30 12:07:02 +0530

    home page created and modified

---
+++ src/pages/HomePage.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Img1 from "./image-1.jpg"
+import Img2 from "./image-2.jpg"
+import Img3 from "./image-3.jpg"
+import "../App.css"
+
+export default function HomePage() {
+  return (
+    <div>
+        <nav>
+            <NavBar />
+        </nav>
+
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src={Img1} class="img-1 d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+                <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src={Img2} class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img src={Img3} class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+
+        <footer>
+            <Footer />
+        </footer>
+    </div>
+  )
+}
