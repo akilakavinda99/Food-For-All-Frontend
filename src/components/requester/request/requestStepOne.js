14be24cb41147dd5d943a10b75ff7b84e92c0cbf
Thushal Shaminda <it20216146@my.sliit.lk>
2022-10-10 00:21:51 +0530

    step one and two modified

--- src/components/requester/request/requestStepOne.js
+++ src/components/requester/request/requestStepOne.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import Footer from '../../Footer'
-import NavBar from '../../NavBar'
-import { Link } from "react-router-dom";
+import React, {useContext} from 'react'
+import { multiStepContext } from '../request/stepContex'
 
-export default function requestStepOne() {
-  
+export default function RequestStepOne() {
+  const { requestData, setRequestData, formErrorsStep1, handleNext } = useContext(multiStepContext)
   return (
     <div>
-      <nav>
-        <NavBar />
-      </nav>
-
       <h4 className="pt-3 ms-4">Request Funds</h4>
       <p className='text-dark font-weight-normal fw-bold mx-4'>Millions of children are dying every day from hunger and diseases that are made worse by undernutrition. 
       No child should ever go hungry or suffer from preventable sickness and disease because their basic nutritional needs go unmet. The statistics are discouraging, 
@@ -26,30 +20,46 @@ export default function requestStepOne() {
           
           <p class="text-center d-flex ms-3 pt-3 text-muted fw-bold "> Requester's psersonal information </p> 
 
-            <div class="row input-group input-group-outline m-0 px-4"> 
-              <div class="col-md-6 mb-4 ps-0"> 
+            <div class="row input-group input-group-outline m-0 pb-4 px-4"> 
+              <div class="col-md-6 ps-0"> 
                 <input class="form-control" 
                 type="text"
-                defaultValue={"thushal"}
-                readOnly
+                placeholder='First name'
+                value={requestData['fname']}
+                onChange={(e) => { setRequestData({ ...requestData, "fname": e.target.value }) }}
                 />
+                <div className="text-danger p-0 m-0">
+                  {formErrorsStep1.fname}
+                </div>
               </div>
-              <div class="col-md-6 mb-4 pe-0">
+              
+
+              <div class="col-md-6 pe-0">
                 <input class="form-control" 
                 type="text"
-                defaultValue={"shaminda"}
-                readOnly
+                placeholder='Last name'
+                value={requestData['lname']}
+                onChange={(e) => { setRequestData({ ...requestData, "lname": e.target.value }) }}
                 />
+                <div className="text-danger p-0 m-0">
+                  {formErrorsStep1.lname}
+                </div>
               </div> 
             </div>
-
-            <div class="input-group input-group-outline mb-4 px-4"> 
+            
+          <div className='row pb-3 px-4'>
+            <div class="input-group input-group-outline"> 
               <input type="text" 
                     placeholder="Telephone number" 
                     class="form-control"
-                    required
-                    /> 
+                    value={requestData['tpno']}
+                    onChange={(e) => { setRequestData({ ...requestData, "tpno": e.target.value }) }}
+                    />
             </div> 
+              <div className="text-danger">
+                  {formErrorsStep1.telephoneNo}
+              </div>
+          </div>
 
             <p class="text-center d-flex ms-3 text-muted fw-bold "> Benificiary's contact information </p> 
 
@@ -58,31 +68,73 @@ export default function requestStepOne() {
                 <input class="form-control" 
                         type="text" 
                         placeholder="Country"
-                        required
+                        value={requestData['country']}
+                        onChange={(e) => { setRequestData({ ...requestData, "country": e.target.value }) }}
                         />
+                      <div className="text-danger">
+                          {formErrorsStep1.country}
+                      </div>
               </div>
+              
               <div class="col-md-6 mb-4 pe-0">
                 <input class="form-control" 
                       type="text" 
                       placeholder="Zip code"
-                      required
+                      value={requestData['zipcode']}
+                      onChange={(e) => { setRequestData({ ...requestData, "zipcode": e.target.value }) }}
                       />
-              </div> 
+                  <div className="text-danger">
+                        {formErrorsStep1.zipcode}
+                  </div>
+              </div>
+               
+            </div>
+            <div className='row pb-3 px-2'>
+            <div class="input-group input-group-outline px-4"> 
+              <input type="text" 
+              placeholder="Address" 
+              class="form-control"
+              value={requestData['address']}
+              onChange={(e) => { setRequestData({ ...requestData, "address": e.target.value }) }}
+              />
+              </div>
+              <div className="text-danger ms-3">
+                  {formErrorsStep1.address}
+              </div>
+            </div> 
+
+
+            <div className='row pb-3 px-2'>
+            <div class="input-group input-group-outline px-4"> 
+              <input type="email" 
+              placeholder="Email Address" 
+              class="form-control"
+              value={requestData['email']}
+              onChange={(e) => { setRequestData({ ...requestData, "email": e.target.value }) }}
+              /> 
+            </div> 
+            <div className="text-danger ms-3">
+                  {formErrorsStep1.email}
+            </div>
             </div>
 
-            <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="text" placeholder="Address" class="form-control"/> 
+            <div className='row pb-3 px-2'>
+            <div class="input-group input-group-outline px-4"> 
+              <input type="text" 
+              placeholder="Telephone number" 
+              class="form-control"
+              value={requestData['contactno']}
+              onChange={(e) => { setRequestData({ ...requestData, "contactno": e.target.value }) }}
+              /> 
             </div> 
-            <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="email" placeholder="Email Address" class="form-control"/> 
-            </div> 
-            <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="text" placeholder="Telephone number" class="form-control"/> 
-            </div> 
+            <div className="text-danger ms-3">
+                  {formErrorsStep1.contactNo}
+            </div>
+            </div>
           
            <div class="row"> 
               <div class="mb-3 d-flex justify-content-center"> 
-                <Link to = "/requester/request/step2"><button class="btn btn-primary d-block "> NEXT </button> </Link>
+                <button type="button" class="btn btn-primary d-block" onClick={handleNext}> NEXT </button>
               </div> 
            </div>
            
@@ -90,10 +142,6 @@ export default function requestStepOne() {
         </div>
       </div>
       </div>
-
-  <footer>
-    <Footer />
-  </footer>
   </div>
   )
 }
--- src/components/requester/request/requestStepTwo.js
+++ src/components/requester/request/requestStepTwo.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { FileUploader } from 'react-drag-drop-files';
+import { multiStepContext } from '../request/stepContex';
 import "../footer.css"
-import Footer from '../../Footer';
-import NavBar from '../../NavBar';
-import swal from "sweetalert";
 
-export default function requestStepTwo() {
+export default function RequestStepTwo() {
+  const { requestData, setRequestData, submitData, requestImage, setRequestImage, formErrorsStep2 } = useContext(multiStepContext);
+  const fileTypes = ["JPEG", "JPG", "PNG"];
 
-  const fundrequest = () => {
-    swal("Succesfully Requested", "", "success");
-  }
+      // Convert file into base64
+      function getBase64(file, cb) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+            cb(reader.result)
+        };
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+    }
+
+    const handleChange = (file) => {
+        setRequestImage(file);
+        getBase64(file, (result) => {
+            // let output = result;
+            setRequestData({ ...requestData, "requestImage": result })
+        })
+    }
+
   return (
     <div>
-      <nav>
-        <NavBar />
-      </nav>
-
       <h4 className="pt-3 ms-4">Request Funds</h4>
       <p className='text-dark font-weight-normal mx-4'>Millions of children are dying every day from hunger and diseases that are made worse by undernutrition.
        No child should ever go hungry or suffer from preventable sickness and disease because their basic nutritional needs go unmet.
@@ -26,30 +39,51 @@ export default function requestStepTwo() {
       
     <div className="card z-index-0 fadeIn3 fadeInBottom ">
     <div className="card-body">
-        <form class="form-control" onSubmit={fundrequest}> 
+        <form class="form-control"> 
           <p class="h3 fw-bold text-center mb-2 pt-4"> Let's make a request </p>
           <p class="text-center d-flex ms-3 pt-4 text-muted fw-bold"> Request details </p> 
             
             <div class="input-group input-group-outline mb-4 px-4"> 
-              <input type="text" placeholder="Request title" class="form-control"/> 
+              <input  type="text" 
+                      placeholder="Request title" 
+                      class="form-control"
+                      value={requestData['title']}
+                      onChange={(e) => { setRequestData({ ...requestData, "title": e.target.value }) }}
+                      /> 
             </div> 
+            <div className="text-danger p-0 m-0">
+                  {formErrorsStep2.title}
+            </div>
+
             <div class="input-group input-group-description input-group-outline mb-4 px-4"> 
-              <input type="text" placeholder="Tell your story (Description)" class="form-control"/> 
+              <input  type="text" 
+                      placeholder="Tell your story (Description)" 
+                      class="form-control"
+                      value={requestData['description']}
+                      onChange={(e) => { setRequestData({ ...requestData, "description": e.target.value }) }}
+                      /> 
             </div> 
+            <div className="text-danger p-0 m-0">
+                  {formErrorsStep2.description}
+            </div>
 
             <p class="text-center d-flex ms-3 text-muted fw-bold"> Attach documents </p>
 
-          <div className='px-5'>
-            <FileUploader />
-            {/* <p>{imageFile ? `File name: ${imageFile.name}` : "No files uploaded yet"}</p> */}
-            {/* <>{imageFile ?  */}
-             <h7 class="text-muted d-flex"> Accepted file types: JPEG, JPG, PNG  </h7>
-            <img className='rounded img-fluid'/>
-          </div>
+            <div className='text-center'>
+                        <div className='d-flex justify-content-center'>
+                            <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+                        </div>
+                        <p>{requestImage ? `File name: ${requestImage.name}` : "No files uploaded yet"}</p>
+                        <p>{requestImage ?
+                            <img className='rounded img-fluid' src={requestData.requestImage} alt={requestImage.name} /> : ""}</p>
+              </div>
+                    <div className="text-danger form-label mb-3">
+                        {formErrorsStep2.requestImage}
+                    </div>
 
            <div class="row"> 
               <div class="mb-3 d-flex justify-content-center"> 
-                <button type='submit' class="btn btn-primary d-block "> SUBMIT </button> 
+                <button type='submit' onClick={submitData} class="btn btn-primary d-block "> SUBMIT </button> 
               </div> 
            </div>
            
@@ -57,10 +91,6 @@ export default function requestStepTwo() {
         </div>
       </div>
       </div>
-
-  <footer>
-    <Footer />
-  </footer>
   </div>
   )
 }
