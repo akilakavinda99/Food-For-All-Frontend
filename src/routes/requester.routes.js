394e114fbb62fb304416c1876ebd84fb27e419ca
Thushal Shaminda <it20216146@my.sliit.lk>
2022-10-12 10:45:51 +0530

    added: new routes

--- src/routes/requester.routes.js
+++ src/routes/requester.routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import RequesterCard from "../components/requester/requesterDashboard";
 import RequesterSignUp from "../components/requester/requesterSignUp";
 import RequesterSignIn from "../components/requester/requesterSignIn"
 import RequesterProfile from "../components/requester/requesterProfile";
@@ -11,20 +10,21 @@ import AllRequests from "../components/requester/allRequests"
 
 import NewRequest from "../pages/requester/newRequest";
 import StepContext from "../components/requester/request/stepContex";
+import ViewRequest from "../components/requester/viewRequest";
 
 export default function Requester() {
   return (
     <>
       <Routes>
-        <Route path="/card" element={<RequesterCard />} />
         <Route path="/signup" element={<RequesterSignUp />} />
         <Route path="/signin" element={<RequesterSignIn />} />
         <Route path="/profile/631aa3f99d2dc36d4c12a8f0" element={<RequesterProfile />} />
         <Route path="/profile/update/631aa3f99d2dc36d4c12a8f0" element={<RequesterProfileUpdate/>} />
         <Route path="/my/requests" element={<MyRequests />} />
-        <Route path ="/all/requets" element={<AllRequests/>} />
+        <Route path ="/all/requests" element={<AllRequests/>} />
 
         <Route path ="/new" element={<StepContext><NewRequest /></StepContext>} />
+        <Route path ="/view/request" element={<ViewRequest/>} />
       </Routes>
     </>
   )
