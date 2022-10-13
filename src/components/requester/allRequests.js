d2b2722fbd472f1f87a4c57117a9847584397c71
Thushal Shaminda <it20216146@my.sliit.lk>
2022-10-14 00:47:22 +0530

    modified: all request page

--- src/components/requester/allRequests.js
+++ src/components/requester/allRequests.js
@@ -8,19 +8,62 @@ export default function AllRequests() {
         <nav>
             <NavBar />
         </nav>
-   
-    <div className='container'>
-        <div class="card-deck">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="..."/>
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div className='container mt-5'>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top"
+                    alt="Hollywood Sign on The Hill" />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                </div>
+                </div>
             </div>
-        </div>
-        </div>
-    </div>
+            <div class="col">
+                <div class="card">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top"
+                    alt="Palm Springs Road" />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top"
+                    alt="Los Angeles Skyscrapers" />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                    additional content.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top"
+                    alt="Skyscrapers" />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                </div>
+                </div>
+            </div>
+            </div>
+
+            </div>
+
     <footer>
         <Footer />
     </footer>
